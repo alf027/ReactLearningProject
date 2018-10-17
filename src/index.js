93460361df689d3b558fbaaee1cc965f6b199de1
John Alfano <john.alfano@zayo.com>
2018-10-17 16:24:51 -0600

    thumbnails working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
-import SearchBar from './components/search_bar'
-
-const API_KEY = 'AIzaSyBbVr5i98MSwin0oFfeeJCpT_dXECtzOBQ'
-
-YTSearch({key: API_KEY, term: 'surfboards'}, data=>{
-     console.log(data)
-    });
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBbVr5i98MSwin0oFfeeJCpT_dXECtzOBQ';
 
 // create a new component. This component should produce some html
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
