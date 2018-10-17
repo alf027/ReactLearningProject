e1c178e09cd94af2fece4b526053f50e904c4014
John Alfano <john.alfano@zayo.com>
2018-10-17 15:52:31 -0600

    implementing YT search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyBbVr5i98MSwin0oFfeeJCpT_dXECtzOBQ'
+
+YTSearch({key: API_KEY, term: 'surfboards'}, data=>{
+     console.log(data)
+    });
 
 // create a new component. This component should produce some html
 
