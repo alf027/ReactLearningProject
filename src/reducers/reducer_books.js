b0af4e7af107ee8fde9a333a0cfc04f0e13c5fb7
John Alfano <alf027@gmail.com>
2018-10-17 21:32:24 -0600

    lots more fun with redux

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    {title: 'Javascript the Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'Javascript the Good Parts', pages: 103},
+    {title: 'Harry Potter', pages: 24},
+    {title: 'Dark Tower', pages: 44},
+    {title: 'Eloquent Ruby', pages: 55}
   ]
 }
